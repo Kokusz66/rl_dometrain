4fae62c9151d1a898a5daa423186e9c493c96ec8
Rolderek <restaslaci@gmail.com>
2026-06-19 17:08:56 +0200

    Mailkit javítva új verziónak megfelelően.

--- Modul_3.Customert.Web/Customers/MailKitEmailSenderServices.cs
+++ Modul_3.Customert.Web/Customers/MailKitEmailSenderServices.cs
@@ -1,29 +1,31 @@
-﻿using MimeKit;
-using System.Net.Mail;
+﻿using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace Modul_3.Customert.Web.Customers
 {
+    //Mailkit javítva a friss verziónak megfelelően
     public class MailKitEmailSenderServices : IEmailSenderService
     {
         public async Task SendEmailAsync(string from, string to, string subject, string body)
         {
             using (SmtpClient client = new SmtpClient())
             {
-                //client.Connect("localhot", 25, false); //ez a régi verzióban kellett
+                await client.ConnectAsync("localhost", 25, false);
+
                 var message = new MimeMessage();
                 message.From.Add(new MailboxAddress(from, from));
                 message.To.Add(new MailboxAddress(to, to));
                 message.Subject = subject;
                 message.Body = new TextPart("plain") { Text = body };
 
-                //await client.Send(message); //a konvertálást a régi és az új verzió között még meg kell néznem!
+                await client.SendAsync(message);
+
                 Console.WriteLine("Email sent!");
 
-                //client.Disconnect(true); //ez is a régi az újban már nem kell
+                await client.DisconnectAsync(true);
             }
         }
 
-        //ez nem is kellene csak hisztizik az interface-re
         public Task SndEmailAsync(string from, string to, string subject, string body)
         {
             throw new NotImplementedException();
