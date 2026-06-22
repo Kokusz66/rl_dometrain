e7144c975e32dd717ae4a73db501e36d4e36e1d9
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-22 14:45:23 +0200

    Testek xUnit

---
+++ AppToTest/AppTOTest.IntegrationsTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace AppTOTest.IntegrationsTests
+{
+    public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram>
+        where TProgram : class
+    {
+
+    }
+}
---
+++ AppToTest/AppTOTest.IntegrationsTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Text.Json;
+using Xunit;
+using Ardalis.HttpClientTestExtensions;
+
+namespace AppTOTest.IntegrationsTests
+{
+    public class WeatherForecastApiGet(CustomWebApplicationFactory<Program> factory) :
+        IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client = factory.CreateClient();
+
+        [Fact]
+        public async Task Returns5Forecasts()
+        {
+            var response = await _client.GetAsync("/weatherforecast");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            var forecast = JsonSerializer.Deserialize<List<WeatherForecast>>(responseString);
+
+            Assert.Equal(5, forecast!.Count);
+        }
+
+
+
+        [Fact]
+        public async Task Returns5ForecastsSmaller()
+        {
+            var forecast = await _client.GetAndDeserializeAsync<List<WeatherForecast>>("/weatherforecast");
+
+            Assert.Equal(5, forecast!.Count);
+        }
+    }
+}
