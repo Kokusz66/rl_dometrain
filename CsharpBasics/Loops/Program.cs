d082ea677d40000f49bb2d996b1e08d894ef2cf9
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 12:32:47 +0200

    Loops

---
+++ CsharpBasics/Loops/Program.cs
@@ -0,0 +1,51 @@
+﻿//Loops while loops
+
+//while (continue)
+//{
+//    //valami kód
+//}
+
+//do
+//{
+//    //kód helye
+//}
+//while (condition)
+
+//csak a feltétel teljesülésekor fog lefutni
+int count = 0;
+while (count < 5)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
+Console.WriteLine($"The total count is {count}!");
+//egyszer mindenképpen lefut
+count = 0;
+do
+{
+    Console.WriteLine(count);
+    count++;
+}
+while (count < 5);
+Console.WriteLine($"The total count of a do while is {count}!");
+
+
+count = 0;
+while (count < 50)
+{
+    if (count == 3)
+    {
+        count++;
+        Console.WriteLine("Skipping 3.");
+        continue;
+    }
+    Console.WriteLine(count);
+    count++;
+
+    if (count == 5)
+    {
+        Console.WriteLine("Simba, it is time...");
+        break;
+    }
+}
