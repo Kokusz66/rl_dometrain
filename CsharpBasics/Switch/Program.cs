834643a66b6d1d8e6826425d3c11d19c28f587fe
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 10:16:21 +0200

    c# basics switch

---
+++ CsharpBasics/Switch/Program.cs
@@ -0,0 +1,46 @@
+﻿
+
+int dayOfWeek = 4;
+switch (dayOfWeek)
+{
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("Wetnesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+    case 7:
+        Console.WriteLine("Sunday");
+        break;
+    default:
+        Console.WriteLine("Invalid day!");
+        break;
+}
+
+Console.WriteLine("This is out from the switch!");
+
+//another innotation:
+string dayOfWeekName = "Sunday";
+string result = dayOfWeekName switch
+{
+    "Monday" => "First day.",
+    "Thuesday" => "Second day.",
+    "Wetnesday" => "Third day.",
+    "Thursday" => "Fourth day.",
+    "Friday" => "Fifth day.",
+    "Saturday" => "Sixth day.",
+    "Sunday" => "Seventh day.",
+    _ => "Invalid day!"
+};
