4fde93ce5803eb3f5120c944105bd2a26f99fb8d
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 13:00:56 +0200

    Simple method

---
+++ CsharpBasics/SimpleMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿//Simple method:
+
+void ThisIsAMethod()
+{
+    //body
+}
+
+ThisIsAMethod();
+
+void PrintSeparator()
+{
+    Console.WriteLine("----------");
+}
+
+Console.WriteLine("Valami");
+Console.WriteLine("Akármi");
+PrintSeparator();
+Console.WriteLine("Valami");
+PrintSeparator();
+Console.WriteLine("Akármi");
+PrintSeparator();
+
+void PrintHeader()
+{
+    PrintSeparator();
+    Console.WriteLine("New Example!");
+    PrintSeparator();
+}
+
+PrintHeader();
