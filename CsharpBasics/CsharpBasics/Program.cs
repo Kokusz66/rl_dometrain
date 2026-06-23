9429fbc0a39ef6ee9ad410a21db33f8d67a8174a
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 08:53:04 +0200

    c# basics strings

---
+++ CsharpBasics/CsharpBasics/Program.cs
@@ -0,0 +1,27 @@
+﻿//
+Console.WriteLine("Hello, World!");
+
+//sime string basics:
+string myString;
+string my_String;
+string MyString;
+
+myString = "Hello, World!";
+string coolString = "Hello, World!";
+coolString = "Goodby, World!";
+
+string firstName = "Jhon";
+string lastName = "Doe";
+string fullName = firstName +  " " + lastName;
+
+Console.WriteLine(fullName);
+
+Console.WriteLine("Tell me your name: ");
+
+myString = Console.ReadLine();
+Console.WriteLine($"Hello {myString}!");
+Console.WriteLine(myString.Length);
+
+Console.WriteLine(myString[0]);
+
+char myChar = 'a';
