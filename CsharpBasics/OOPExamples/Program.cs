c6a30f5b20e988327c9afb0f175aa84d4f2662d1
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 13:40:17 +0200

    OOP alapok

---
+++ CsharpBasics/OOPExamples/Program.cs
@@ -0,0 +1,32 @@
+﻿//OOP:
+
+OurClass ourClass = new OurClass();
+
+ourClass.Greetings();
+Console.WriteLine(ourClass.ExampleFunction());
+
+public class OurClass
+{
+
+    public OurClass()
+    {
+        
+    }
+    public void Greetings()
+    {
+        Console.WriteLine("Greetings Commander!");
+    }
+
+    public int ExampleFunction()
+    {
+        return 42;
+    }
+
+
+}
+
+
+
+
+
+
