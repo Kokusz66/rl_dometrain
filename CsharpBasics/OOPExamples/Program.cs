0a3bb418c5d57227f6435e4b3715ab98100d7445
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 14:19:07 +0200

    Referencia és objektum oda vissza

--- CsharpBasics/OOPExamples/Program.cs
+++ CsharpBasics/OOPExamples/Program.cs
@@ -1,9 +1,25 @@
 ﻿//OOP:
 
 OurClass ourClass = new OurClass();
+OurClass ourClass2 = new OurClass();
+OurClass ourClass3 = ourClass2;
 
 ourClass.Greetings();
 Console.WriteLine(ourClass.ExampleFunction());
+
+List<int> myNumbers1 = new List<int> { 1, 2, 3 };
+List<int> myNumbers2 = new List<int> { 1, 2, 3 };
+
+Console.WriteLine(myNumbers1 == myNumbers2); //false más a memória címe stb...
+
+//referenciából értékadás és egy másolat új értékadása nem változtatja meg az eredetit, példákkal bemutatva
+
+List<string> wordsOut = new List<string> { "alma", "korte", "barack" };
+ourClass.PrintList(wordsOut);
+
+
+ourClass.ChangeReference(wordsOut);
+ourClass.PrintList(wordsOut);
 
 public class OurClass
 {
@@ -22,6 +38,22 @@ public class OurClass
         return 42;
     }
 
+    public void ChangeReference(List<string> words)
+    {
+        words.Add("from");
+        words.Add("Dev");
+        words.Add("Leader");
+    }
+
+    public void PrintList(List<string> list)
+    {
+        Console.WriteLine("\n");
+        foreach (string element in list)
+        {
+            Console.WriteLine(element);
+        }
+        Console.WriteLine("\n");
+    }
 
 }
 
