5b8619666d88b7be2ee19278c836625b485392e6
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 12:39:27 +0200

    forLoops

---
+++ CsharpBasics/ForLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿//for loop:
+
+for (int i=0; i<10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+    {
+        Console.WriteLine("See ya next time!");
+        break;
+    }
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+    {
+        Console.WriteLine("Not again!");
+        continue;
+    }
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n");
