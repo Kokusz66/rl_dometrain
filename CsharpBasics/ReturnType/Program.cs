41bed3c18f80d33f96827300a31b57077cbfd37e
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 13:13:57 +0200

    return type

---
+++ CsharpBasics/ReturnType/Program.cs
@@ -0,0 +1,17 @@
+﻿//Return type:
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+int sum = Add(5, 3);
+
+int x = 5;
+int y = 3;
+int sum2 = Add(y, x);
+
+int sum3 = Add(Add(1, 2), Add(3, 4));
+
+Console.WriteLine(sum);
+Console.WriteLine(sum2);
+Console.WriteLine(sum3);
