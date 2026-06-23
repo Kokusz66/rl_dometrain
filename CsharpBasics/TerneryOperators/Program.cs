000d36a78d4be195b31556720919b78275461651
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 10:04:44 +0200

    Ternery operators

---
+++ CsharpBasics/TerneryOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿//Ternery Operators
+//
+
+int x = 10;
+string result = x > 5
+    ? "x is greater than 5"
+    : "x is less then 5";
+Console.WriteLine(result);
+
+result = x == 10
+    ? "x is equal to 10"
+    : "x is not equal to 10";
+Console.WriteLine(result);
+
+result = x < 20
+    ? "x is less than 20"
+    : "x is greater than 20";
+Console.WriteLine(result);
