6fd860918f4951c492558b109a9dc729fa85c8d9
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 09:58:39 +0200

    if vége

--- CsharpBasics/ControlFlow/Program.cs
+++ CsharpBasics/ControlFlow/Program.cs
@@ -44,3 +44,36 @@ else
 {
     Console.WriteLine("Trick question?!"); //sosem fut le mert a bool csak true vagy false lehet
 }
+
+int number = 1;
+if (number < 5)
+{
+    Console.WriteLine("The number is lower than 5.");
+}
+else if (number == 5)
+{
+    Console.WriteLine("The number is equeal to 5.");
+}
+else
+{
+    Console.WriteLine("The number is greater than 5.");
+}
+
+number = 4;
+if (number >= 1 && number <= 5)
+{
+    Console.WriteLine("The number is bettwen the range 1-5.");
+}
+else
+{
+    Console.WriteLine("The number is not bettwen the range 1-5.");
+}
+
+if (number < 1 || number > 5)
+{
+    Console.WriteLine("The number is not bettwen 2-4.");
+}
+else
+{
+    Console.WriteLine("The number is bettwen 2-4.");
+}
