9550ac9a9230bcf00586f322a250624bc884df7c
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 12:48:42 +0200

    Foreach

---
+++ CsharpBasics/ForEachLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿//Foreach:
+
+int[] numbers = { 1, 2, 3, 4, 5 };
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("\n");
+
+List<string> words = new List<string>
+{
+    "red",
+    "green",
+    "blue",
+};
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+Console.WriteLine("\n");
+
+Dictionary<string, int> ages = new()
+{
+    {"Alice", 25 },
+    {"Bob", 24 },
+    {"Charlie", 26 },
+};
+
+foreach (var person in ages)
+{
+    Console.WriteLine($"{person.Key} is {person.Value} years old.");
+}
+
+//break és continue ugyanúgy működik itt is
