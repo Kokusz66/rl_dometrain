0d688511d43ead43669ecf686b4f97c809a40a7a
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-06-23 13:06:33 +0200

    Arguments

---
+++ CsharpBasics/Arguments/Program.cs
@@ -0,0 +1,22 @@
+﻿//Arguments
+
+void PrintSeparator()
+{
+    Console.WriteLine("----------");
+}
+
+
+void PrintHeader(string name)
+{
+    PrintSeparator();
+    Console.WriteLine(name);
+    PrintSeparator();
+}
+
+void MyMethod(string name, int age)
+{
+    PrintHeader(name);
+    Console.WriteLine(age);
+}
+
+MyMethod("Józsi", 20);
