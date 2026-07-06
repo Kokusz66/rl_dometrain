50d599ffae15374171755e61f4fc5884ee2babf3
DevAdmin <DevAdmin@DESKTOP-MQQ6CNJ>
2026-07-06 15:26:19 +0200

    Demo

--- First/obj/Debug/net9.0/First.AssemblyInfo.cs
+++ First/obj/Debug/net9.0/First.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46f46c74e4840bad255d8c27f4df6cd20cc5bf21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cbe5c270583ab44209cd2f859a586326f3c834c")]
 [assembly: System.Reflection.AssemblyProductAttribute("First")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
